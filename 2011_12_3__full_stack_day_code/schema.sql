d8835880789bbbbe4ef5ff0cf179c25ceed4f446
Mallioch <eric.sowell@gmail.com>
2011-12-03 09:20:08 -0600

    schema!

---
+++ 2011_12_3__full_stack_day_code/schema.sql
@@ -0,0 +1,45 @@
+USE [ToDoStuff]
+GO
+
+/****** Object:  Table [dbo].[Task]    Script Date: 12/03/2011 09:04:34 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Task]') AND type in (N'U'))
+DROP TABLE [dbo].[Task]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TaskType]') AND type in (N'U'))
+DROP TABLE [dbo].[TaskType]
+GO
+
+
+USE [ToDoStuff]
+GO
+
+/****** Object:  Table [dbo].[Task]    Script Date: 12/03/2011 09:04:34 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Task](
+	[TaskId] [int] PRIMARY KEY IDENTITY,
+	[Description] [nvarchar](200) NOT NULL,
+	[CreateDate] [datetime] NOT NULL DEFAULT(getdate())
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[TaskType](
+	[TaskTypeId] [int] NOT NULL,
+	[TaskTypeName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_TaskType] PRIMARY KEY CLUSTERED 
+(
+	[TaskTypeId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+INSERT INTO TaskType (TaskTypeId, TaskTypeName) VALUES (1, 'at home')
+INSERT INTO TaskType (TaskTypeId, TaskTypeName) VALUES (2, 'at work')
+INSERT INTO TaskType (TaskTypeId, TaskTypeName) VALUES (3, 'at the store')
