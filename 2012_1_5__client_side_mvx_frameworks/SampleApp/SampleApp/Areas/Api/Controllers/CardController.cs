e17f70be4d0ab756fd8d098ccde622463158923a
Eric Sowell <eric.sowell@gmail.com>
2011-12-21 16:43:59 -0600

    card view done in Spine. Woot!

---
+++ 2012_1_5__client_side_mvx_frameworks/SampleApp/SampleApp/Areas/Api/Controllers/CardController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleApp.Code;
+using System.Dynamic;
+using System.Threading;
+
+namespace SampleApp.Areas.Api.Controllers
+{
+    public class CardController : Controller
+    {
+        public CardController()
+        {
+            _repo = new CardRepository();
+        }
+
+        private CardRepository _repo;
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //Thread.Sleep(2000);
+
+            var cards = _repo.All();
+
+            var vm = new List<Card>();
+            foreach (dynamic card in cards)
+            {
+                vm.Add(new Card
+                    {
+                        id = Convert.ToInt32(card.CardId),
+                        recto = card.Recto,
+                        verso = card.Verso,
+                        correctCount = card.TimesCorrect,
+                        incorrectCount = card.TimesIncorrect,
+                        isArchived = Convert.ToBoolean(card.IsArchived)
+                    });
+            }
+
+            return Json(vm, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Insert(Card card)
+        {
+            dynamic data = new ExpandoObject();
+            data.Recto = card.recto;
+            data.Verso = card.verso;
+            data.TimesCorrect = card.correctCount;
+            data.TimesIncorrect = card.incorrectCount;
+            data.IsArchived = card.isArchived;
+
+            _repo.Save(data);
+
+            return new EmptyResult();
+        }
+    }
+}
--- 2012_1_5__client_side_mvx_frameworks/SampleApp/SampleApp/Content/Spine/my_app.js
+++ 2012_1_5__client_side_mvx_frameworks/SampleApp/SampleApp/Content/Spine/my_app.js
@@ -1,1 +1,98 @@
-﻿
+﻿(function (exports) {
+
+    var Card = Spine.Model.setup('Card', ['recto', 'verso']);
+    Card.extend(Spine.Model.Ajax);
+    Card.extend({
+        url: '/api/card'
+    });
+    Card.bind('refresh', function () {
+        console.log('refreshing', Card.all());
+    });
+
+    var cards;
+    var createControllers = function () {
+
+        //Cards controller
+        var Cards = Spine.Controller.create({
+            init: function () {
+                Card.bind('refresh change', this.proxy(this.start));
+            },
+
+            events: {
+                'click #forward': 'forward',
+                'click #back': 'back',
+                'click #card': 'flip'
+            },
+
+            start: function () {
+                cardList = Card.all();
+                cardList.sort(function () { return 0.5 - Math.random() });
+                this.index = 0;
+                this.side = 'recto';
+                this.updateCard();
+            },
+
+            forward: function () {
+                this.index++;
+                this.side = 'recto';
+
+                if (this.index === cardList.length)
+                    this.index = 0;
+                this.updateCard();
+            },
+
+            back: function () {
+                this.index--;
+                this.side = 'recto';
+
+                if (this.index === -1)
+                    this.index = cardList.length - 1;
+                this.updateCard();
+            },
+
+            flip: function () {
+                if (this.side === 'verso')
+                    this.side = 'recto';
+                else
+                    this.side = 'verso';
+                this.updateCard();
+            },
+
+            updateCard: function () {
+                this.model = cardList[this.index];
+                this.render();
+            },
+
+            render: function () {
+                $('#card', this.el).html('<p>' + this.model[this.side] + '</p>');
+            }
+        });
+
+        //AppController
+        window.App = Spine.Controller.create({
+            el: $('#app'),
+
+            elements: {
+                '#cardHolder': 'cardHolderEl',
+                '#addForm': 'addFormEl'
+            },
+
+            init: function () {
+                this.cardHolder = Cards.init({
+                    el: this.cardHolderEl
+                });
+
+                Card.fetch();
+            }
+
+        }).init();
+    }
+
+    var cardList;
+    var index = 0;
+
+    $(document).ready(function () {
+        createControllers();
+    });
+
+})(this);
