872b8ac8cb8a92df0ffd71f9e932fdb2a160a18e
mallioch <eric.sowell@gmail.com>
2012-08-15 07:46:25 -0500

    .net sample

---
+++ 2012_8_14__all_about_websockets/socketconsole/socketconsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.WebSockets;
+
+namespace socketconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+
+            Console.WriteLine("Press <ENTER> to exit...");
+            Console.ReadLine();
+        }
+
+        private static void Start()
+        {
+            string uri = String.Format("http://localhost:4000/foo/");
+            var listener = new WebsocketListener();
+            listener.Listen(uri);
+            
+
+        }
+
+        
+    }
+}
